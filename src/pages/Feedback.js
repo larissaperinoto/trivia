f16bfc85fc27b93d62645277a99acfdb0a31732a
Larissa Perinoto <perinotolarissa@gmail.com>
2022-08-09 18:13:01 -0300

    [Grupo 6] - Requisito 15.

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
+import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+
 import Header from '../components/Header';
 
 class Feedback extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      redirect: false,
+    };
+  }
+
+  handleClick = () => {
+    this.setState({ redirect: true });
+  }
+
   render() {
     const { assertions } = this.props;
-    console.log(assertions);
+    const { redirect } = this.state;
     const spots = 3;
     return (
       <>
@@ -16,6 +30,15 @@ class Feedback extends React.Component {
             ? <span>Could be better...</span>
             : <span>Well Done!</span>}
         </div>
+        <button
+          type="button"
+          id="btn-play-again"
+          data-testid="btn-play-again"
+          onClick={ () => this.handleClick() }
+        >
+          Play Again
+        </button>
+        { redirect && <Redirect to="/" /> }
       </>
     );
   }
@@ -27,6 +50,5 @@ const mapStateToProps = (state) => ({
 Feedback.propTypes = {
   assertions: PropTypes.string,
 }.isRequired;
-
 
 export default connect(mapStateToProps)(Feedback);
