46b09bf48edfbff4177f435466ad2989f8f38d03
BrandtLC <lucasbrandtcosta@gmail.com>
2022-08-10 18:17:06 -0300

    completa requisito 19

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { PropTypes } from 'prop-types';
 import { connect } from 'react-redux';
 
+import md5 from 'crypto-js/md5';
+
 class Ranking extends React.Component {
   handleClick = () => {
     const { history } = this.props;
@@ -10,6 +12,10 @@ class Ranking extends React.Component {
 
   render() {
     const { name, score, assertions } = this.props;
+    const localStorageData = localStorage.getItem('ranking');
+    const rankingList = localStorageData && localStorageData !== ''
+    && JSON.parse(localStorageData)
+      .sort((a, b) => b.score - a.score);
     return (
       <div>
         <h1 data-testid="ranking-title">Ranking</h1>
@@ -21,6 +27,16 @@ class Ranking extends React.Component {
         >
           Início
         </button>
+        <ul>
+          {localStorageData && rankingList.map((c, i) => (
+            <li key={ i }>
+              <img src={ `https://www.gravatar.com/avatar/${md5(c.email).toString()}` } alt={ `Imagem de ${c.name}` } />
+              <p>Player:</p>
+              <p data-testid={ `player-name-${i}` }>{ c.name }</p>
+              <p>Score:</p>
+              <p data-testid={ `player-score-${i}` }>{ c.score }</p>
+            </li>))}
+        </ul>
       </div>
     );
   }
